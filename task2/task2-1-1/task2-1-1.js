4e8af2ec5128b3cfa1bad2ab1d82f85474c9dcef
x13500x <38378439+x13500x@users.noreply.github.com>
2018-10-18 00:12:52 +0800

    task完成玩家分配及身份查看

---
+++ task2/task2-1-1/task2-1-1.js
@@ -0,0 +1,34 @@
+//分配玩家
+function people() {
+    var num1=document.getElementById("num1");
+    var num2=document.getElementById("num2");
+    var num3=document.getElementById("num3").value;
+    num1.value=Math.floor(num3 / 3);
+    num2.value=Math.ceil(num3 *2 /3);
+    console.log(num3);
+
+}
+function change() {
+    var num3=document.getElementById("num3").value;
+    if(num3<4||num3>18){
+        alert("请输入正确的人数");
+    }
+    else {
+        people();
+    }
+}
+function num() {
+    var killer=document.getElementById("num1").value;
+    var citizen=document.getElementById("num2").value;
+    var player=document.getElementById("num3").value;
+    console.log(killer);
+    console.log(citizen);
+    console.log(player);
+    if (player>=4&&player<=18){
+        window.location.href="task2-1-2.html";
+        sessionStorage.setItem("killer",killer);//存储杀手数量
+        sessionStorage.setItem("citizen",citizen);//存储平民数量
+        sessionStorage.setItem("player",player);//存储玩家数量
+    }
+}
+
